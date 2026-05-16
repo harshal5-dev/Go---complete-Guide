4460356367ac7e19da5c711cdfb31ec0cd9616d2
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-05-16 14:38:50 +0530

    Add user input and output to profit calculator
    Add descriptive labels to investment calculator output

--- go basics/profit calculator/profit_calculator.go
+++ go basics/profit calculator/profit_calculator.go
@@ -3,6 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Enter revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter tax rate: ")
+	fmt.Scan(&taxRate)
+
 	fmt.Println("Profit Calculator")
+
+	earningsBeforeTax := revenue - expenses
+	profit := earningsBeforeTax * (1 - taxRate/100)
+	ratio := profit / revenue
+	fmt.Println("Earnings before tax: ", earningsBeforeTax)
+	fmt.Println("Profit: ", profit)
+	fmt.Println("Ratio: ", ratio)
 
 }
