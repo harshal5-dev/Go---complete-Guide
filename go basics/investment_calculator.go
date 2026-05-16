1e5cedb6d61238bccc59f90f041b6f67bcdcee38
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-05-16 13:21:48 +0530

    Replace hardcoded values with user input prompts

--- go basics/investment_calculator.go
+++ go basics/investment_calculator.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	years := 10.0
+	const inflationRate = 6.5
+	var investmentAmount float64
+	var years float64
 	expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
