90f21c9aa6fcb61773143fb4a35d41394f3434a8
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-05-17 12:46:26 +0530

    file write done for profit calculator

--- go basics/profit calculator/profit_calculator.go
+++ go basics/profit calculator/profit_calculator.go
@@ -1,20 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func writeResultInFile(earningsBeforeTax, profit, ratio float64) {
+	outputText := fmt.Sprintf("Earnings before tax: %.1f\n", earningsBeforeTax)
+	outputText += fmt.Sprintf("Profit: %.1f\n", profit)
+	outputText += fmt.Sprintf("Ratio:%.2f\n ", ratio)
+	os.WriteFile("result.txt", []byte(outputText), 0644)
+}
 
 func main() {
 
-	revenue := getUserInput("Enter revenue: ")
-	expenses := getUserInput("Enter expenses: ")
-	taxRate := getUserInput("Enter tax rate: ")
-
 	fmt.Println("Profit Calculator")
+
+	revenue, err := getUserInput("Enter revenue: ")
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err := getUserInput("Enter expenses: ")
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Enter tax rate: ")
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		return
+	}
 
 	earningsBeforeTax, profit, ratio := calculateValues(revenue, expenses, taxRate)
 
 	fmt.Printf("Earnings before tax: %.1f\n", earningsBeforeTax)
 	fmt.Printf("Profit: %.1f\n", profit)
 	fmt.Printf("Ratio:%.2f\n ", ratio)
+	writeResultInFile(earningsBeforeTax, profit, ratio)
 }
 
 func calculateValues(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -24,9 +56,15 @@ func calculateValues(revenue, expenses, taxRate float64) (float64, float64, floa
 	return earningsBeforeTax, profit, ratio
 }
 
-func getUserInput(text string) float64 {
+func getUserInput(text string) (float64, error) {
 	var userInput float64
 	fmt.Print(text)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		outputError := fmt.Sprintf("Invalid Input %.1f, please enter value greater than 0", userInput)
+		return 0, errors.New(outputError)
+	}
+
+	return userInput, nil
 }
