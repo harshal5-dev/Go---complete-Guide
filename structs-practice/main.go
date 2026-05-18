703fc4da0dfaa466593883027ff9d5d651b337db
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-05-18 10:49:23 +0530

    generic typing done

--- structs-practice/main.go
+++ structs-practice/main.go
@@ -10,6 +10,33 @@ import (
 	"example.com/note/todo"
 )
 
+func add[T int | float64 | string](a, b T) T {
+
+	return a + b
+	// aInt, aIsInt := a.(int)
+	// bInt, bIsInt := b.(int)
+
+	// if aIsInt && bIsInt {
+	// 	return aInt + bInt
+	// }
+
+	// aFloat, aIsFloat := a.(float64)
+	// bFloat, bIsFloat := b.(float64)
+
+	// if aIsFloat && bIsFloat {
+	// 	return aFloat + bFloat
+	// }
+
+	// aString, aIsString := a.(string)
+	// bString, bIsString := b.(string)
+
+	// if aIsString && bIsString {
+	// 	return aString + bString
+	// }
+
+	// return nil
+}
+
 type Saver interface {
 	Save() error
 }
@@ -96,6 +123,10 @@ func main() {
 	printSomething("HI Any value")
 	printSomething(21)
 	printSomething(22.3)
+
+	result := add(11, 22)
+
+	fmt.Println(result)
 
 	title, content := getNoteData()
 	todoText := getTodoData()
