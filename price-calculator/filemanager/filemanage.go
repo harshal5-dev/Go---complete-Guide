edde571b85ce71e491a23044b2c9b3084251d774
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-05-28 10:59:00 +0530

    implement price calculation with tax inclusion and file management

---
+++ price-calculator/filemanager/filemanage.go
@@ -0,0 +1,53 @@
+package filemanager
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		file.Close()
+		return nil, errors.New("Could not open file!")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+
+	if err != nil {
+		file.Close()
+		return nil, errors.New("Reading the file content failed.")
+	}
+
+	file.Close()
+	return lines, nil
+}
+
+func WriteJSON(path string, data any) error {
+	file, err := os.Create(path)
+
+	if err != nil {
+		return errors.New("Failed to create File!")
+	}
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+
+	if err != nil {
+		file.Close()
+		return errors.New("Failed to convert data to JSON")
+	}
+
+	file.Close()
+	return nil
+}
--- price-calculator/prices/prices.go
+++ price-calculator/prices/prices.go
@@ -1,9 +1,34 @@
 package prices
 
+import (
+	"fmt"
+
+	"example.com/price-calculator/conversion"
+	"example.com/price-calculator/filemanager"
+)
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
+}
+
+func (job *TaxIncludedPriceJob) LoadData() {
+	lines, err := filemanager.ReadLines("prices.txt")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	prices, err := conversion.StringToFloat(lines)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	job.InputPrices = prices
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
@@ -11,4 +36,18 @@ func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 		InputPrices: []float64{10, 20, 30, 40},
 		TaxRate:     taxRate,
 	}
+}
+
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadData()
+	result := make(map[string]string)
+	key := fmt.Sprintf("%0.2f", job.TaxRate)
+
+	for _, price := range job.InputPrices {
+		ans := price * (1 + job.TaxRate)
+		result[key] = fmt.Sprintf("%0.2f", ans)
+	}
+
+	job.TaxIncludedPrices = result
+	filemanager.WriteJSON(fmt.Sprintf("result_%0.1f.json", job.TaxRate*100), job)
 }
